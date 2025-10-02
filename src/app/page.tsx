a4da017a85d9bec976b22b2b3a35c7181cae5299
Rafael Ventura <onlupy@gmail.com>
2025-10-02 21:34:29 +0000

    vc criou 2 pastas de icon uma icon e outra icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,19 +39,6 @@ const AdBanner = () => {
   );
 };
 
-
-const DragonIcon = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    fill="currentColor"
-    {...props}
-  >
-    <path d="M17.828 10.243c.44-.439.44-1.152 0-1.591L15.145 6c-1.112-1.111-2.39-1.667-3.834-1.667-1.445 0-2.723.556-3.835 1.667L5.01 8.409c-.44.439-.44 1.152 0 1.591L6.701 11.7c1.334 1.334 3.326 2.068 5.299 2.068s3.965-.734 5.299-2.068l.529-.529zM12 15.75c-2.33 0-4.403-.89-5.96-2.448l-.759-.759a.75.75 0 010-1.06L7.43 9.334a.75.75 0 011.06 0l.759.759c.74.74 1.77 1.157 2.867 1.157s2.127-.417 2.867-1.157l.759-.759a.75.75 0 011.06 0l2.149 2.149a.75.75 0 010 1.06l-.759.759A7.468 7.468 0 0112 15.75zm-3.3-8.083c.318 0 .578-.26.578-.578V5.01a.579.579 0 00-.578-.578H8.12a.579.579 0 00-.578.578v2.08c0 .318.26.578.578.578h.577zm7.18 0c.318 0 .578-.26.578-.578V5.01a.579.579 0 00-.578-.578h-.578a.579.579 0 00-.578.578v2.08c0 .318.26.578.578.578h.578zm-1.125 10.515a.75.75 0 01.75-.75h.375a.75.75 0 010 1.5h-.375a.75.75 0 01-.75-.75zM12 18.375c.414 0 .75.336.75.75v3.125a.75.75 0 01-1.5 0v-3.125a.75.75 0 01.75-.75z" />
-  </svg>
-);
-
-
 export default function Home() {
   const [name, setName] = useState<string>('');
 
@@ -85,7 +72,7 @@ export default function Home() {
         <AdBanner />
         <CardHeader className="text-center">
           <CardTitle className="text-4xl font-bold font-headline tracking-tighter flex justify-center items-center gap-4">
-             <DragonIcon className="w-12 h-12 text-primary" />
+             <Wand2 className="w-12 h-12 text-primary" />
              Nome Fantástico
           </CardTitle>
           <CardDescription className="text-lg">
